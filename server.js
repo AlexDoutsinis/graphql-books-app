22d686ea6110cfc94d8b83a0c6d60b7ec1761738
Alex <Alexiosdut@gmail.com>
2019-08-18 10:05:32 +0300

    added a single book query

--- server.js
+++ server.js
@@ -50,7 +50,13 @@ const AuthorType = new GraphQLObjectType({
   description: "This represents an author of a book",
   fields: () => ({
     id: { type: GraphQLNonNull(GraphQLInt) },
-    name: { type: GraphQLNonNull(GraphQLString) }
+    name: { type: GraphQLNonNull(GraphQLString) },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve: author => {
+        return books.filter(book => book.authorId === author.id);
+      }
+    }
   })
 });
 
@@ -58,6 +64,14 @@ const RootQueryType = new GraphQLObjectType({
   name: "Query",
   description: "Root Query",
   fields: () => ({
+    book: {
+      type: BookType,
+      description: "A single book",
+      args: {
+        id: { type: GraphQLInt }
+      },
+      resolve: (parent, args) => books.find(book => book.id === args.id)
+    },
     books: {
       type: new GraphQLList(BookType),
       description: "List of All books",
